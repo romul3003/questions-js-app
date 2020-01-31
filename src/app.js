21b16a347421ecb375ed5c8ba1063a822f32df00
Roman Zakrevskyi <romul3003@gmail.com>
2020-01-31 18:41:07 +0200

    Add form validation.

--- src/app.js
+++ src/app.js
@@ -1,2 +1,30 @@
+import {isValid} from './utils'
 import './styles.css'
-console.log('test app')
+
+const form = document.getElementById('form')
+const input  = document.querySelector('#question-input')
+const submitBtn = document.querySelector('#submit')
+
+form.addEventListener('submit', submitFormHandler)
+input.addEventListener('input', () => {
+	submitBtn.disabled = !isValid(input.value)
+})
+
+function submitFormHandler(event) {
+  event.preventDefault()
+
+	if (isValid(input.value)) {
+		const question = {
+			text: input.value.trim(),
+			date: new Date().toJSON()
+		}
+
+		submitBtn.disabled = true
+		// Async request to server to save question
+		console.log('Question', question)
+
+		input.value = ''
+		input.className = ''
+		submitBtn.disabled = false
+	}
+}
